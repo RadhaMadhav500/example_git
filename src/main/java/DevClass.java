9b816571d59b040d9be707aeebf40cd2aa1f0630
user19 <user19@clipcoupon.com>
2022-09-17 17:53:36 -0400

    commit to feature1

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("Dev Class Hello1");
+        System.out.println("Dev Class Hello1 from feature1 branch");
     }
 }
